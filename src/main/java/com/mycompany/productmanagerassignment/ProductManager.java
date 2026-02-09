f97f89ca24c2c94341a1624bfc837aaf9da6b57a
Zeel Kachhiya <kachhiyz@sheridancollege.ca>
2026-02-09 17:17:29 -0500

    Initial commit by Zeel Kachhiya

---
+++ src/main/java/com/mycompany/productmanagerassignment/ProductManager.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.productmanagerassignment;
+
+/**
+ *
+ * @author Zeel
+ */
+public class ProductManager {
+    public double calculateTotal(double price, int quantity) {
+        return price * quantity;
+    }
+}
